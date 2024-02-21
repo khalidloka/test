623fdabefb0e5983c5014493ec4e5abfd0efbf1c
khalidloka <96070737+khalidloka@users.noreply.github.com>
2024-02-21 10:58:36 +0100

    prev

--- app/page.jsx
+++ app/page.jsx
@@ -2,19 +2,15 @@ import CreateAccount from "@/components/CreateAccount";
 import WelcomeBack from "@/components/WelcomeBack";
 import Login from "@/components/Login";
 import HelloFreind from "@/components/HelloFriend";
-import { AuthProvider, useAuth } from "@/components/AuthContext";
 
 
 
 export default function Home() {
-  let  { SignInClicked , SignInHandler } =  useContext(signinContext);
-  let  { SignUpClicked , SignUpHandler } =  useContext(showContext);
+  
   return (<div className="flex">
-      <showContext.Provider >
    <WelcomeBack />
        <CreateAccount/>
        
-    </showContext.Provider>
  
   </div>);
 }
--- components/HelloFriend.jsx
+++ components/HelloFriend.jsx
@@ -1,25 +1,19 @@
-import React from "react";
+
+'use client'
+import React, { useState } from "react";
 
 export default function WelcomeBack() {
-  const showContext = createContext();
   const [SignUpClicked, setSignUpClicked] = useState(true);
-  let { setSignInClicked } = useContext(signinContext);
-  const SignUpHandler = () => {
-    setSignInClicked(false);
-    setSignUpClicked(true)
-
-  };
+ 
 
   return (
     <>
-    <showContext.Provider value={{ SignUpClicked,SignUpHandler }}>
     <div className="flex justify-center items-center h-[100vh] w-[45vw] bg-[#7919F7]">
         <div >
       <h1 className="flex justify-center text-white font-extrabold text-4xl ml-4 my-12 ">Hello Freind !</h1>
       <h2 className="flex justify-center text-gray-400 text-xl mb-20">Provide your personal information to<br />commence your journey with us</h2>
-      <div className="flex justify-center"><h2 className="flex justify-center items-center text-white font-semibold border-4 border-white rounded-full text-2xl h-14 w-56 hover:bg-white hover:text-[#7919F7] hover:" onClick={SignUpHandler}>Sign Up</h2></div>
+      <div className="flex justify-center"><h2 className="flex justify-center items-center text-white font-semibold border-4 border-white rounded-full text-2xl h-14 w-56 hover:bg-white hover:text-[#7919F7] hover:" >Sign Up</h2></div>
    </div> </div>
-   </showContext.Provider>
     </>
   );
 }
